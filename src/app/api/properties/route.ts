9936df5ca12bc2fddf4d6dabfb4c382ffa1264c6
yecarter13 <pouabegille@gmail.com>
2026-07-05 23:56:09 +0100

    trigger rebuild

--- src/app/api/properties/route.ts
+++ src/app/api/properties/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
+
 export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
